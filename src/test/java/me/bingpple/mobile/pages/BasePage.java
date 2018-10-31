2a84b33883b4885b3a9dd90f47dbac1877cd250a
[bingcai] <[bingcai@catalyst.net.nz]>
2018-11-01 12:57:37 +1300

    fix an openjdk issue

--- src/test/java/me/bingpple/mobile/pages/BasePage.java
+++ src/test/java/me/bingpple/mobile/pages/BasePage.java
@@ -1,12 +1,12 @@
 package me.bingpple.mobile.pages;
 
-import io.appium.java_client.pagefactory.AndroidFindBy;
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 
 public class BasePage extends PageObject {
 
-    @AndroidFindBy(id = "comments")
+    @FindBy(id = "comments")
     public WebElementFacade commentBox;
 
 }
